2c61672babea1e32c023f9f745dc6350ff407484
USUARIO <ignaciojose9@hotmail.com>
2019-09-05 20:14:13 -0300

    menu principal

--- screens/MenuPrincipal.js
+++ screens/MenuPrincipal.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
         flex: 1,
         // flexDirection: 'row',
         alignItems: 'center',
-        position: 'absolute'
+        justifyContent: 'space-between'
     },
     boton: {
         flex: 1,
